cf3248141ca6a226a1c29f5c8c38d95f2d127c86
Grupith <dylanhkoss@gmail.com>
2025-05-06 14:58:46 -0500

    refactor: simplify dashboard greeting and improve layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,8 +4,11 @@ import { useAuth } from "@/contexts/AuthContext";
 export default function Page() {
   const { user } = useAuth();
   return (
-    <div className="p-4 dark:bg-gray-900 dark:text-gray-200">
-      <h1>Welcome to the Dashboard, {user?.displayName || user?.email}!</h1>
+    <div className=" dark:bg-gray-900 dark:text-gray-200">
+      <h1>Dashboard</h1>
+      <p className="text-2xl font-bold">
+        Welcome {user?.displayName || "unknown name"}
+      </p>
     </div>
   );
 }
